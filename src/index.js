6fc10d569eb672a55565c084f70e590610dafbe1
Jon Kuperman <jon.kuperman@gmail.com>
2017-08-27 14:52:37 -0700

    basic setup

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import babylon from 'babylon';
+
+const code = `function inbox() {
+  this.doSomething = function() { /* ... */ }
+  this.doSomethingElse = function() { /* ... */ }
+
+  // after initializing the component
+  this.after('initialize', function() {
+    this.on('click', this.doSomething);
+    this.on('mouseover', this.doSomethingElse);
+  });
+}`;
+
+const ast = babylon.parse(code);
+console.log(ast);
